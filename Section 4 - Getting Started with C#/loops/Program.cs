644b462617ff713f67bb712054ce2752a6f8e0e1
Johan Scheepers <johanscheepers14@gmail.com>
2023-05-23 18:34:31 +0200

    loops
    Experimented using while- and for-loops.

---
+++ Section 4 - Getting Started with C#/loops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Loops
+{
+    class Progam
+    {
+        public static void Main(String[] args)
+        {
+            List<string> collection = new List<string>() {"hello", "people", "of", "the", "world" };
+            for(int i=0; i < collection.Count; i++)
+            {
+                Console.WriteLine(collection[i]);
+            }
+
+            int count = collection.Count;
+
+            while(count > 0)
+            {
+                Console.WriteLine(count);
+                count--;
+            }
+        }
+    }
+}
