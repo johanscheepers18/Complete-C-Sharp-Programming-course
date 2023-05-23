6e0a3bf28bfd2202220eeabd09f2c4f113c381f2
Johan Scheepers <johanscheepers14@gmail.com>
2023-05-23 18:06:01 +0200

    C# variables
    Learned about different variables you get in C#.

---
+++ Section 4 - Getting Started with C#/Variables/Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Variables
+{ 
+    
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            //int is any whole number either positive or negative
+            int intNumber1 = 33;
+            int intNumber2 = -55;
+
+            //float is any floating point number as should end with a 'f'
+            float floatNumber1 = 812.33f;
+            float floatNumber2 = -66.54f;
+
+            double doubleNumber1 = 55.08;
+            double doubleNumber2 = -78.32;
+
+            //string could be either a single character, word or a sentence.
+            //characters must be contained inside double quotation marks " ".
+            string stringLetter = "a";
+            string stringWord = "House";
+            string stringSentence = "This is not 'Hello World'";
+
+            //char is a single charater contained in single quotation marks ' '.
+            char testChar = 'G';
+
+            //bool is a value either true or false.
+            bool testBool = true;
+        }
+    }
+}
