aee0eb6b8aa44ec4df62ea98ec9705222c21175d
Johan Scheepers <johanscheepers14@gmail.com>
2023-05-23 18:24:50 +0200

    Conditional Statements
    tested the abilities of the if, else if, else statements in C#.

---
+++ Section 4 - Getting Started with C#/if,else-if,else/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConditionalSatements
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int option = 1;
+
+            if (option == 0)
+            {
+                string output = "Your chosen option is '0'";
+                Console.WriteLine(output);
+            }
+
+            else if (option == 1)
+            {
+                string output = "Your chosen option is '1'";
+                Console.WriteLine(output);
+            }
+
+            else
+            {
+                string output = "Your chose option is invalid";
+                Console.WriteLine(output);
+            }
+
+        }
+    }
+}
